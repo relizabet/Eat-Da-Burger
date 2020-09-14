426ea67189a035df90471f6f530c7794c2d4c58c
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-14 17:19:19 -0400

    handlebars responding

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,12 @@ const express = require("express");
 const burger = require("../models/burger");
 // module.exports = routes.firstRoute;
 
+// attribute
+const test = [{ thing1: "Hello" }];
+
 module.exports = (app) => {
   app.get("/first", function (req, res) {
     console.log(`route working`);
-    res.render("index", thing);
+    res.render("index", test[0]);
   });
 };
