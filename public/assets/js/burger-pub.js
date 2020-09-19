7e7fa60141efd29f4cb83b85a60ff6ad67c52c65
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-19 16:32:47 -0400

    moved layouts around

--- public/assets/js/burger-pub.js
+++ public/assets/js/burger-pub.js
@@ -1,5 +1,6 @@
 $(function () {
-  $(".devour_button").on("click", function (event) {
+  // test
+  $(".order_button").on("click", function (event) {
     console.log(event);
     console.log("this is working");
   });
