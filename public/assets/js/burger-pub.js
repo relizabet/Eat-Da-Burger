f4e2329318f37d54348c76c624b0762a185fee0b
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-09-19 17:11:16 -0400

    idk api not working

--- public/assets/js/burger-pub.js
+++ public/assets/js/burger-pub.js
@@ -1,7 +1,38 @@
 $(function () {
-  // test
-  $(".order_button").on("click", function (event) {
-    console.log(event);
-    console.log("this is working");
+  $(".eat-button").on("click", function (e) {
+    const id = $(this).data("id");
+
+    $.ajax(`/api/burgers/${id}`, {
+      type: "PUT",
+      data: { devoured: 1 },
+    }).then(function () {
+      console.log("eaten");
+      location.reload();
+    });
+  });
+
+  $(".check").on("submit", function (e) {
+    e.preventDefault();
+    const createBurger = {
+      burger_to_eat: $(".burger_input").val().trim(),
+      devoured: 0,
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: createBurger,
+    }).then(function () {
+      console.log("created");
+    });
+  });
+
+  $(".delete-button").on("click", function (e) {
+    const id = $(this).data("id");
+
+    $.ajax(`/api/burgers/${id}`, {
+      type: "DELETE",
+    }).then(function () {
+      location.reload();
+    });
   });
 });
